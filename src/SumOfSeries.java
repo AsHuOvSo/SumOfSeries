681c5692df8a2f9b5aa82bf5ae3e81c15b50a4a9
AsHuOvSo <asta@avra.dk>
2020-09-08 12:01:04 +0200

    Initial commit

---
+++ src/SumOfSeries.java
@@ -0,0 +1,9 @@
+public class SumOfSeries {
+    //Starting program
+    public static void main(String[] args) {
+        //Figuring out answer
+        int answer = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+        //Printing answer
+        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = " + answer);
+    }
+}
